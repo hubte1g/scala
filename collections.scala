687722f683a46706b84753c95659aa6f494cb634
hubte1g <lickteig.joshua@gmail.com>
2016-06-21 11:01:07 -0500

    Update collections.scala

--- collections.scala
+++ collections.scala
@@ -1,6 +1,9 @@
 scala> ((1 to 10) fold "") ((s1, s2) => s"$s1 - $s2")
 res2: Any = " - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10"
 
+scala> val f = ((1 to 10) fold "::") ((a11,a12) => s"$a11 - $a12")
+f: Any = :: - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10
+
 scala> (1 to 25).toArray.foldLeft(0)(_ + _)
 res8: Int = 325
 
