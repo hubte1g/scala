1c73ad418a1a8da25b2f04a338fe15ecf47b425c
hubte1g <lickteig.joshua@gmail.com>
2016-06-23 15:33:56 -0500

    Update obj.scala

--- obj.scala
+++ obj.scala
@@ -3,3 +3,17 @@ object <object name> {
       def <method>(): Unit =
    }
 }
+
+// Objects are used to hold single instances of a class. Often used for factories.
+
+object Timer {
+   var count = 0
+   
+   def currentCount(): Long = {
+      count += 1
+      count
+   }
+} // defined module Timer
+
+Timer.currentCount() // res10: Long = 1
+
