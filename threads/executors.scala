54682190697d6af2f6f9a26ffff4e49b0055666a
hubte1g <lickteig.joshua@gmail.com>
2016-07-11 15:17:21 -0500

    Update executors.scala

--- threads/executors.scala
+++ threads/executors.scala
@@ -1,1 +1,45 @@
+import java.net.{Socket, ServerSocket}
+import java.util.concurrent.{Executors, ExecutorService}
+import java.util.Date
 
+class Handler(socket: Socket) extends Runnable {
+  def message = (Thread.currentThread.getName() + "\n").getBytes
+
+  def run() {
+    socket.getOutputStream.write(message)
+    socket.getOutputStream.close()
+  }
+}
+
+class NetworkService(port: Int, poolSize: Int) extends Runnable {
+  val serverSocket = new ServerSocket(port)
+  val pool: ExecutorService = Executors.newFixedThreadPool(poolSize)
+
+  def run() {
+    try {
+      while (true) {
+        // This will block until a connection comes in.
+        val socket = serverSocket.accept()
+        pool.execute(new Handler(socket))
+      }
+    } finally {
+      pool.shutdown()
+    }
+  }
+}
+
+(new NetworkService(2020, 3)).run // port, number of threads
+
+// local transcript to show how the threads are used
+
+$ nc localhost 2020  // host port
+pool-1-thread-1
+
+$ nc localhost 2020
+pool-1-thread-2
+
+$ nc localhost 2020
+pool-1-thread-3
+
+$ nc localhost 2020
+pool-1-thread-1
