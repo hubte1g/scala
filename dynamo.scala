a288b8b026d4439d191129ea9e8c01fcc95f4e8f
hubte1g <lickteig.joshua@gmail.com>
2019-05-14 09:23:22 -0700

    Create dynamo.scala

---
+++ dynamo.scala
@@ -0,0 +1,24 @@
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Table, Item, DynamoDB}
+import com.amazonaws.services.dynamodbv2.model._
+import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClient, AmazonDynamoDBClientBuilder}
+
+def dynamoDB: AmazonDynamoDB = {
+    val client = new AmazonDynamoDBClient()
+    client.setEndpoint("https://dynamodb.us-east-1.amazonaws.com")
+    client
+  }
+
+  //dynamoDB.describeTable("table_name")
+
+  import scala.collection.JavaConverters._
+
+//  val key = Map(
+//    "source_id" -> new AttributeValue().withS("999999999"),
+//    "source_cd" -> new AttributeValue().withS("category")
+//  ).asJava
+//  val identifier_map = Map("#language" -> "language").asJava
+//  //val clientConfig = new ClientConfiguration()
+//  val client = AmazonDynamoDBClientBuilder.standard().build()
+//  val request = new GetItemRequest().withTableName("table_name").withKey(key)//.withProjectionExpression(retrnCols).withExpressionAttributeNames(identifier_map)
+//  val response = client.getItem(request)
+//  val s = response.getItem()
